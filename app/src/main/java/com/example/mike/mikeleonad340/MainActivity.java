7460d0ef3f91395a8d54dd72c86d7f6d11753934
MikeJLeon <michaeljleon@live.com>
2018-04-23 23:27:10 -0700

    Assignment 3 - Added Toast buttons

--- app/src/main/java/com/example/mike/mikeleonad340/MainActivity.java
+++ app/src/main/java/com/example/mike/mikeleonad340/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.mike.mikeleonad340;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.mike.MESSAGE";
@@ -22,5 +24,27 @@ public class MainActivity extends AppCompatActivity {
         String message = input.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
+    }
+
+    public void toast1(View view){
+        Context context = getApplicationContext();
+        String text = "Sup";
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+    public void toast2(View view){
+        Context context = getApplicationContext();
+        String text = "Pizza is great";
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+    public void toast3(View view){
+        Context context = getApplicationContext();
+        String text = "Android is fun";
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
     }
 }
---
+++ app/src/main/java/com/example/mike/mikeleonad340/list.java
@@ -0,0 +1,13 @@
+package com.example.mike.mikeleonad340;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class list extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+    }
+}
